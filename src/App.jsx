9a69dd6a034f7cdc251813fd18a2e42d3e910f26
Vando <vandomeri@gmail.com>
2025-09-03 10:36:36 +0300

    first_react_36

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Counter from "./components/Counter";
+
+export default function App() {
+
+
+
+  return (
+
+    <div>
+
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+
+    </div>
+
+  )
+
+}
